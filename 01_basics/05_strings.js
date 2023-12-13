8556c57463283b1dcb8e3739d9450e745b611ac6
Krishnakant Sadhu <sadhukrishnakant@gmail.com>
2023-12-13 23:22:14 +0530

    string manipulation

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,35 +1,37 @@
-const name = "hitesh"
+const name = "krishnakant"
 const repoCount = 50
 
-// console.log(name + repoCount + " Value");
+// console.log(name + repoCount + " Value"); //not recommended
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //this is recommended way
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+const gameName = new String('krish-hc-com')
 
-const gameName = new String('hitesh-hc-com')
-
-// console.log(gameName[0]);
+// console.log(gameName[0]); //character at index
 // console.log(gameName.__proto__);
 
 
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+// console.log(gameName.length);   // length is property, hence no ()
+// console.log(gameName.toUpperCase()); //toUpperCase is method, hence ()
+// console.log(gameName.charAt(2));  // index start with zero
+// console.log(gameName.indexOf('t'));  // -1 mean not found
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+//const newString = name.substring(2, 4)  //sub string start index , end index
+// const newString = name.substring(3)  //sub string start index, till end
 
-const anotherString = gameName.slice(-8, 4)
+const myStr = "0123456789"
+const anotherString = myStr.slice(2, 7)
+const anotherString1 = myStr.substring(2, 7)
 console.log(anotherString);
+console.log(anotherString1);
 
 const newStringOne = "   hitesh    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
-console.log(url.replace('%20', '-'))
+// console.log(url.replace('%20', '-'))
 
-console.log(url.includes('sundar'))
+// console.log(url.includes('sundar'))
 
-console.log(gameName.split('-'));
+// console.log(gameName.split('-'));
